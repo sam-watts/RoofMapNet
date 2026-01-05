6b0511d299195437e5d7c29cdf8a7ec2b45a57dc
Sam Watts <sam.watts445@gmail.com>
2026-01-05 14:57:04 +0000

    make installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="roofmapnet",
+    version="0.1.0",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    author="Your Name",
+    description="RoofMapNet - Roof mapping using deep learning",
+    license="MIT",
+)
